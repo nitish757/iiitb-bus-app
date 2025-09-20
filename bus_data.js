62b3681e36a24021ba5e8a91dd3093e47c8e1bf9
Nitish Mahapatre <nitish468mahapatre@gmail.com>
2025-09-21 02:53:18 +0530

    Update bus_data.js

--- bus_data.js
+++ bus_data.js
@@ -2,253 +2,253 @@ const scheduleData = {
     "uniworldToIiitb": [
         {
             "time": "7:00 AM",
-            "days": ["B1", "B1", "B1", "B1", "B1", "B1", null]
+            "days": [null, "B1", "B1", "B1", "B1", "B1", "B1"]
         },
         {
             "time": "7:30 AM",
-            "days": ["B2", "B2", "B2", "B2", "B2", "B2", null]
+            "days": [null, "B2", "B2", "B2", "B2", "B2", "B2"]
         },
         {
             "time": "8:00 AM",
-            "days": ["B1 B3", "B1 B3", "B1 B3", "B1 B3", "B1 B3", "B1 B3", null]
+            "days": [null, "B1 B3", "B1 B3", "B1 B3", "B1 B3", "B1 B3", "B1 B3"]
         },
         {
             "time": "9:00 AM",
-            "days": ["B2", "B2", "B2", "B2", "B2", "B2", "B4"]
+            "days": ["B4", "B2", "B2", "B2", "B2", "B2", "B2"]
         },
         {
             "time": "9:15 AM",
-            "days": ["B1", "B1", "B1", "B1", "B1", "B1", null]
+            "days": [null, "B1", "B1", "B1", "B1", "B1", "B1"]
         },
         {
             "time": "9:30 AM",
-            "days": ["B3", "B3", "B3", "B3", "B3", "B3", null]
+            "days": [null, "B3", "B3", "B3", "B3", "B3", "B3"]
         },
         {
             "time": "9:30 AM",
-            "days": ["B4", "B4", "B4", "B4", "No Bus", "B4", null]
+            "days": [null, "B4", "B4", "B4", "B4", "No Bus", "B4"]
         },
         {
             "time": "10:15 AM",
-            "days": ["B1", "B1", "B1", "B1", "No Bus", "B4", "B1"]
+            "days": ["B1", "B1", "B1", "B1", "B1", "No Bus", "B4"]
         },
         {
             "time": "11:00 AM",
-            "days": ["B2", "B2", "B2", "B2", "B2", "B2", null]
+            "days": [null, "B2", "B2", "B2", "B2", "B2", "B2"]
         },
         {
             "time": "11:15 AM",
-            "days": ["B1", "B1", "B1", "B1", "No Bus", "B1", null]
+            "days": [null, "B1", "B1", "B1", "B1", "No Bus", "B1"]
         },
         {
             "time": "12:00 PM",
-            "days": ["B3", "B3", "B3", "B3", "B1", "B3", null]
+            "days": [null, "B3", "B3", "B3", "B3", "B1", "B3"]
         },
         {
             "time": "12:30 PM",
-            "days": ["B2", "B2", "B2", "B2", "No Bus", "B2", null]
+            "days": [null, "B2", "B2", "B2", "B2", "No Bus", "B2"]
         },
         {
             "time": "1:00 PM",
-            "days": ["B3", "B3", "B3", "B3", "B3", "B3", "B4"]
+            "days": ["B4", "B3", "B3", "B3", "B3", "B3", "B3"]
         },
         {
             "time": "1:45 PM",
-            "days": ["B1", "B1", "B1", "B1", "B2", "B1", null]
+            "days": [null, "B1", "B1", "B1", "B1", "B2", "B1"]
         },
         {
             "time": "2:00 PM",
-            "days": ["B2", "B2", "B2", "B2", null, "B2", null]
+            "days": [null, "B2", "B2", "B2", "B2", null, "B2"]
         },
         {
             "time": "2:15 PM",
-            "days": ["B3", "B3", "B3", "B3", "B3", "B3", null]
+            "days": [null, "B3", "B3", "B3", "B3", "B3", "B3"]
         },
         {
             "time": "2:45 PM",
-            "days": ["B1", "B1", "B1", "B1", "B1", "B1", null]
+            "days": [null, "B1", "B1", "B1", "B1", "B1", "B1"]
         },
         {
             "time": "3:00 PM",
-            "days": ["B2", "B2", "B2", "B2", "B2", "B2", null]
+            "days": [null, "B2", "B2", "B2", "B2", "B2", "B2"]
         },
         {
             "time": "3:15 PM",
-            "days": [null, null, null, null, "B3", null, null]
+            "days": [null, null, null, null, null, "B3", null]
         },
         {
             "time": "3:45 PM",
-            "days": ["B3", "B3", "B3", "B3", "B1", "B3", null]
+            "days": [null, "B3", "B3", "B3", "B3", "B1", "B3"]
         },
         {
             "time": "4:15 PM",
-            "days": ["B2", "B2", "B2", "B2", "No Bus", "B2", null]
+            "days": [null, "B2", "B2", "B2", "B2", "No Bus", "B2"]
         },
         {
             "time": "4:45 PM",
-            "days": ["B4", "B4", "B4", "B4", "B2", "B4", "B4"]
+            "days": ["B4", "B4", "B4", "B4", "B4", "B2", "B4"]
         },
         {
             "time": "5:00 PM",
-            "days": ["B1", "B1", "B1", "B1", "B1", "B1", null]
+            "days": [null, "B1", "B1", "B1", "B1", "B1", "B1"]
         },
         {
             "time": "5:30 PM",
-            "days": ["B2", "B2", "B2", "B2", "No Bus", "B2", null]
+            "days": [null, "B2", "B2", "B2", "B2", "No Bus", "B2"]
         },
         {
             "time": "6:00 PM",
-            "days": ["B4", "B4", "B4", "B4", "B2", "B4", null]
+            "days": [null, "B4", "B4", "B4", "B4", "B2", "B4"]
         },
         {
             "time": "6:30 PM",
-            "days": [null, null, null, null, null, null, "B4"]
+            "days": ["B4", null, null, null, null, null, null]
         },
         {
             "time": "7:00 PM",
-            "days": ["B4", "B4", "B4", "B4", "No Bus", "B4", null]
+            "days": [null, "B4", "B4", "B4", "B4", "No Bus", "B4"]
         },
         {
             "time": "8:00 PM",
-            "days": ["B4", "B4", "B4", "B4", "B3", "B4", null]
+            "days": [null, "B4", "B4", "B4", "B4", "B3", "B4"]
         },
         {
             "time": "9:30 PM",
-            "days": ["B4", "B4", "B4", "B4", "B2", "B4", null]
+            "days": [null, "B4", "B4", "B4", "B4", "B2", "B4"]
         },
         {
             "time": "10:30 PM",
-            "days": ["B4", "B4", "B4", "B4", "B3", "B4", null]
+            "days": [null, "B4", "B4", "B4", "B4", "B3", "B4"]
         }
     ],
     "iiitbToUniworld": [
         {
             "time": "6:30 AM",
-            "days": ["B1", "B1", "B1", "B1", "B1", "B1", null]
+            "days": [null, "B1", "B1", "B1", "B1", "B1", "B1"]
         },
         {
             "time": "7:00 AM",
-            "days": ["B2", "B2", "B2", "B2", "B2", "B2", null]
+            "days": [null, "B2", "B2", "B2", "B2", "B2", "B2"]
         },
         {
             "time": "7:30 AM",
-            "days": ["B1", "B1", "B1", "B1", "B1", "B1", null]
+            "days": [null, "B1", "B1", "B1", "B1", "B1", "B1"]
         },
         {
             "time": "8:00 AM",
-            "days": ["B3", "B3", "B3", "B3", "B3", null, null]
+            "days": [null, "B3", "B3", "B3", "B3", "B3", null]
         },
         {
             "time": "8:30 AM",
-            "days": ["B2", "B2", "B2 B3", "B2 B3", "B4", null, null]
+            "days": [null, "B2", "B2", "B2 B3", "B2 B3", "B4", null]
         },
         {
             "time": "8:45 AM",
-            "days": ["B1", "B1", "B1", "B1", null, null, null]
+            "days": [null, "B1", "B1", "B1", "B1", null, null]
         },
         {
             "time": "9:00 AM",
-            "days": ["B3", "B3", "B3 B1", "B3 B1", "No Bus", "B4", null]
+            "days": [null, "B3", "B3", "B3 B1", "B3 B1", "No Bus", "B4"]
         },
         {
             "time": "9:45 AM",
-            "days": ["B1", "B1", "B1", "B1", "No Bus", "B1", "B4"]
+            "days": ["B4", "B1", "B1", "B1", "B1", "No Bus", "B1"]
         },
         {
             "time": "10:30 AM",
-            "days": ["B2", "B2", "B2", "B2", "B2", null, null]
+            "days": [null, "B2", "B2", "B2", "B2", "B2", null]
         },
         {
             "time": "10:45 AM",
-            "days": ["B1", "B1", "B1", "B1", "No Bus", null, null]
+            "days": [null, "B1", "B1", "B1", "B1", "No Bus", null]
         },
         {
             "time": "11:30 AM",
-            "days": ["B3", "B3", "B3", "B3", "B1", null, null]
+            "days": [null, "B3", "B3", "B3", "B3", "B1", null]
         },
         {
             "time": "12:00 PM",
-            "days": ["B2", "B2", "B2", "B2", "No Bus", null, null]
+            "days": [null, "B2", "B2", "B2", "B2", "No Bus", null]
         },
         {
             "time": "12:30 PM",
-            "days": ["B3", "B3", "B3", "B3", "B3", "B3", "B4"]
+            "days": ["B4", "B3", "B3", "B3", "B3", "B3", "B3"]
         },
         {
             "time": "1:15 PM",
-            "days": ["B1", "B1", "B1", "B1", "No Bus", null, null]
+            "days": [null, "B1", "B1", "B1", "B1", "No Bus", null]
         },
         {
             "time": "1:15 PM",
-            "days": ["B2", "B2", "B2", "B2", "B2", "B2", null]
+            "days": [null, "B2", "B2", "B2", "B2", "B2", "B2"]
         },
         {
             "time": "1:45 PM",
-            "days": ["B3", "B3", "B3", "B3", "B3", null, null]
+            "days": [null, "B3", "B3", "B3", "B3", "B3", null]
         },
         {
             "time": "2:15 PM",
-            "days": ["B1", "B1", "B1", "B1", "B1", null, null]
+            "days": [null, "B1", "B1", "B1", "B1", "B1", null]
         },
         {
             "time": "2:30 PM",
-            "days": ["B2", "B2", "B2", "B2", "B2", null, null]
+            "days": [null, "B2", "B2", "B2", "B2", "B2", null]
         },
         {
             "time": "2:45 PM",
-            "days": [null, null, null, null, "B3", null, null]
+            "days": [null, null, null, null, null, "B3", null]
         },
         {
             "time": "3:15 PM",
-            "days": ["B3", "B3", "B3", "B3", "B3", "B1", null]
+            "days": [null, "B3", "B3", "B3", "B3", "B3", "B1"]
         },
         {
             "time": "3:45 PM",
-            "days": ["B2", "B2", "B2", "B2", "No Bus", null, null]
+            "days": [null, "B2", "B2", "B2", "B2", "No Bus", null]
         },
         {
             "time": "4:15 PM",
-            "days": ["B4", "B4", "B4", "B4", "B4", "B2", "B4"]
+            "days": ["B4", "B4", "B4", "B4", "B4", "B4", "B2"]
         },
         {
             "time": "4:30 PM",
-            "days": ["B1", "B1", "B1", "B1", "B1", null, null]
+            "days": [null, "B1", "B1", "B1", "B1", "B1", null]
         },
         {
             "time": "5:00 PM",
-            "days": ["B2", "B2", "B2", "B2", "No Bus", null, null]
+            "days": [null, "B2", "B2", "B2", "B2", "No Bus", null]
         },
         {
             "time": "5:30 PM",
-            "days": ["B4", "B4", "B4", "B4", "B4", "B2", null]
+            "days": [null, "B4", "B4", "B4", "B4", "B4", "B2"]
         },
         {
             "time": "6:00 PM",
-            "days": [null, null, null, null, null, "B4", null]
+            "days": [null, null, null, null, null, null, "B4"]
         },
         {
             "time": "6:30 PM",
-            "days": ["B4", "B4", "B4", "B4", "No Bus", null, null]
+            "days": [null, "B4", "B4", "B4", "B4", "No Bus", null]
         },
         {
             "time": "7:30 PM",
-            "days": ["B4", "B4", "B4", "B4", "B3", "B4", null]
+            "days": [null, "B4", "B4", "B4", "B4", "B3", "B4"]
         },
         {
             "time": "8:00 PM",
-            "days": [null, null, null, null, null, "B4", null]
+            "days": [null, null, null, null, null, null, "B4"]
         },
         {
             "time": "9:00 PM",
-            "days": ["B4", "B4", "B4", "B4", "B4", "B2", null]
+            "days": [null, "B4", "B4", "B4", "B4", "B4", "B2"]
         },
         {
             "time": "10:00 PM",
-            "days": ["B4", "B4", "B4", "B4", "B4", "B3", null]
+            "days": [null, "B4", "B4", "B4", "B4", "B4", "B3"]
         },
         {
             "time": "11:00 PM",
-            "days": ["B4", "B4", "B4", "B4", "B4", "B3", null]
+            "days": [null, "B4", "B4", "B4", "B4", "B4", "B3"]
         }
     ]
 }
